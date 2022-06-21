e6334a3e96d17c8dc7350d97aab33c25b0836290
vaishudurgekar <vaishnavigd.xworkz@gmail.com>
2022-06-21 19:26:05 +0530

    commiting Method file

---
+++ Substraction.java
@@ -0,0 +1,17 @@
+class Substraction{
+
+  public static void main(String a[]){
+  
+     substraction(478,57);
+	 substraction(234,567);
+	 substraction(223,98);
+	 substraction(45,34);
+	
+  }
+  
+  
+  public static void substraction(int a, int b){
+  System.out.println(a-b);
+  }
+
+}
