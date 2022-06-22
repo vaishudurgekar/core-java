53bcedd27d5f74d8133d41a56f7d81f36431a8df
vaishudurgekar <vaishnavigd.xworkz@gmail.com>
2022-06-22 19:49:30 +0530

    commiting BookMyMovie file

---
+++ BookMyMovieTester.java
@@ -0,0 +1,11 @@
+class BookMyMovieTester{
+	
+ public static voud main(String a[]){
+	 
+    BookMyMovie bookMyShow = new BookMyMovie();
+	bookMyShow.showTime(20, "Devi", "om");
+	
+    bookMyShow.buySnacks(5, "Samosa");
+ }
+
+}
